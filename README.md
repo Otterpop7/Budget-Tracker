cdcd1cbf6873ffe837e65d85e59f87359c83a104
Otterpop7 <Jordanotterson7@gmail.com>
2022-05-16 16:54:29 -0600

    new repository finished code deployed heroku

---
+++ README.md
@@ -0,0 +1,17 @@
+# Budget-Tracker
+
+Progressive Web Application
+
+```
+AS AN avid traveler
+I WANT to be able to track my withdrawals and deposits with or without a data/internet connection
+SO THAT my account balance is accurate when I am traveling
+```
+
+## Installation Instructions
+
+```
+npm init, npm i, npm i compression, npm i express, npm i mongoose, npm i morgan
+```
+
+[Heroku Budget-Tracker](https://damp-eyrie-83952.herokuapp.com/)
---
+++ public/service-worker.js
@@ -0,0 +1,66 @@
+const app_prefix = 'Budget-';
+const version = 'version_01';
+const cache_name = app_prefix + version;
+const files_to_cache = [
+    "./index.html",
+    "./manifest.json",
+    "./css/styles.css",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png",
+    "./js/index.js",
+    "./js/idb.js"
+];
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cache_name).then(function (cache) {
+            console.log('installing cache : ' + cache_name)
+            return cache.addAll(files_to_cache)
+        })
+    )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(app_prefix);
+            });
+            // add current cache name to keeplist
+            cacheKeeplist.push(cache_name);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
